2848711385475a78cac30cb89e3530a54d3deda9
anouar garbaa <g.anouar@yahoo.com>
2025-03-28 15:48:49 +0100

    More views

--- server.ts
+++ server.ts
@@ -4,10 +4,6 @@ import { generateFakeProducts } from "./utils/fakeData";
 import ProductService from "./services/ProductService";
 import ProductController from "./controllers/productController";
 
-
-
-
-
 const app = express();
 
 app.use(express.json());
@@ -21,9 +17,7 @@ app.set('views', path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 
-app.get('/', (req, res) =>{
-  res.render('index')
-})
+
 
 const fakeProductsData = generateFakeProducts();
 
@@ -32,23 +26,38 @@ const productService = new ProductService(fakeProductsData);
 const productController = new ProductController(productService);
 console.log();
 
-app.get("/", (req, res) => {
-  res.send(`<h1>Hello Express.js</h1>`);
-});
+// app.get("/", (req, res) => {
+//   res.send(`<h1>Hello Express.js</h1>`);
+// });
 
-app.get("/products", (req, res) => productController.getProducts(req, res));
+app.get('/products', (req, res) =>{
+  res.render('products')
+})
+
+app.get("/api/products", (req, res) => productController.getProducts(req, res));
  
   //const queryParams = req.query;
   //console.log(queryParams); // { filter: 'title' } //?filter=title
 
-app.get("/products/:id", (req, res) => productController.getProductById(req, res));
+app.get("/api/products/:id", (req, res) => productController.getProductById(req, res));
   //console.log(req.params); // { id: '242'}
 
-app.post("/products", (req, res) => productController.createProduct(req, res));
+app.post("/api/products", (req, res) => productController.createProduct(req, res));
 
-app.patch("/products/:id", (req, res) => productController.updateProduct(req, res));
+app.patch("/api/products/:id", (req, res) => productController.updateProduct(req, res));
 
-app.delete("/products/:id", (req, res) => productController.deleteProduct(req, res));
+app.delete("/api/products/:id", (req, res) => productController.deleteProduct(req, res));
+
+app.get('/', (req, res) =>{
+  res.render('index')
+})
+
+app.get('*', (req, res) =>{
+  res.render('notFound')
+})
+
+
+
 
 const PORT: number = 5000;
 app.listen(PORT, () => {
